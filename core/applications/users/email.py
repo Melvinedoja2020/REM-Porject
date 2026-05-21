aad8d8354a69a94ebe04174c906e26166389f3d5
Gentility01 <mastergentility5gmail.com>
2026-05-21 18:50:01 +0100

    Hotfixes

--- core/applications/users/email.py
+++ core/applications/users/email.py
@@ -1,7 +1,6 @@
 from templated_mail.mail import BaseEmailMessage
 
 from core.applications.users.token import default_token_generator
-from core.helpers.emails import build_email_context
 
 
 class ActivationEmail(BaseEmailMessage):
@@ -10,37 +9,26 @@ class ActivationEmail(BaseEmailMessage):
     def get_context_data(self):
         # ActivationEmail can be deleted
         context = super().get_context_data()
+
         user = context.get("user")
         context["token"] = default_token_generator.make_token(user)
-        return build_email_context(context)
+        return context
 
 
 class ConfirmationEmail(BaseEmailMessage):
     template_name = "email/confirmation.html"
 
-    def get_context_data(self):
-        return build_email_context(super().get_context_data())
-
 
 class PasswordResetEmail(ActivationEmail):
     template_name = "email/password_reset.html"
-
-    def get_context_data(self):
-        return build_email_context(super().get_context_data())
 
 
 class PasswordChangedConfirmationEmail(BaseEmailMessage):
     template_name = "email/password_changed_confirmation.html"
 
-    def get_context_data(self):
-        return build_email_context(super().get_context_data())
-
 
 class UsernameChangedConfirmationEmail(BaseEmailMessage):
     template_name = "email/username_changed_confirmation.html"
-
-    def get_context_data(self):
-        return build_email_context(super().get_context_data())
 
 
 class UsernameResetEmail(ActivationEmail):
@@ -51,4 +39,4 @@ class UsernameResetEmail(ActivationEmail):
 
         user = context.get("user")
         context["token"] = default_token_generator.make_token(user)
-        return build_email_context(context)
+        return context
