94dc341914ba39660fc229d2dd6034df58aa7112
Gentility01 <mastergentility5gmail.com>
2026-05-22 16:14:32 +0100

    Hotfixes

--- config/settings/production.py
+++ config/settings/production.py
@@ -97,7 +97,7 @@ CLOUDINARY_STORAGE = {
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 DEFAULT_FROM_EMAIL = env(
     "DJANGO_DEFAULT_FROM_EMAIL",
-    default="EMAIL_HOST_USER",
+    default="real estate market place <EMAIL_HOST_USER>",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
